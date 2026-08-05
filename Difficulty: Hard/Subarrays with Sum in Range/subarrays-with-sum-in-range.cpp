c37db0180b2570a4fc337cd2fe9dabe98bcf4978
NIKHIL DHAKAR <nikhildhakad81@gmail.com>
2026-08-05 17:54:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Subarrays with Sum in Range/subarrays-with-sum-in-range.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+  
+    long long countAtMost(vector<int>& arr, long long k) {
+        if (k < 0) return 0;
+
+        long long ans = 0;
+        long long sum = 0;
+        int left = 0;
+
+        for (int right = 0; right < arr.size(); right++) {
+            sum += arr[right];
+
+            while (sum > k) {
+                sum -= arr[left++];
+            }
+
+            ans += (right - left + 1);
+        }
+
+        return ans;
+    }
+
+    long long countSubarray(vector<int>& arr, int l, int r) {
+        return countAtMost(arr, r) - countAtMost(arr, (long long)l - 1);
+    }
+};
