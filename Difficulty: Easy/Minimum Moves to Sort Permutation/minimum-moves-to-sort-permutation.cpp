c239df65c0b4b2e6bc4af95474b33b38ba756718
NIKHIL DHAKAR <nikhildhakad81@gmail.com>
2026-08-25 01:42:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum Moves to Sort Permutation/minimum-moves-to-sort-permutation.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minMoves(vector<int>& arr) {
+        int n = arr.size();
+
+        vector<int> pos(n + 1);
+
+        for (int i = 0; i < n; i++) {
+            pos[arr[i]] = i;
+        }
+
+        int longest = 1;
+        int current = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (pos[i] < pos[i + 1]) {
+                current++;
+            } else {
+                current = 1;
+            }
+
+            longest = max(longest, current);
+        }
+
+        return n - longest;
+    }
+};
